b4dbc64baa6cc79950f0a5452048313d5025fe0e
Cale McDonnell <90876298+crmcdonn@users.noreply.github.com>
2023-03-29 21:28:16 -0400

    Player Join Updates
    - Glory card gets added to players hand on sit down.
    - Captain card gets placed on players board faced down on sit down. (Only works for red)

--- bartley.lua
+++ bartley.lua
@@ -1,15 +1,16 @@
 --[[ Lua code. See documentation: https://api.tabletopsimulator.com/ --]]
 
 players = {"Red", "Yellow", "Green", "Blue"}
-
---[[ Gets the game objects for easy use. --]]
-function onLoad()
-    gloryCardDeck = getObjectFromGUID("c2369c")
-end
+--Global.grid.visible = true
 
 --[[ The onLoad event is called after the game save finishes loading. --]]
 function onLoad()
+    gloryCardDeck = getObjectFromGUID("c2369c")
+    gloryCardDeck.randomize()
 
+    captainCardDeck = getObjectFromGUID("5c23eb")
+    captainCardDeck.randomize()
+    
 end
 
 --[[ The onUpdate event is called once per frame. --]]
@@ -22,5 +23,18 @@ end
 function onPlayerChangeColor(player_color)
     if player_color ~= "Grey" then
         getObjectFromGUID("c2369c").deal(1, player_color)
+        --captainZone = getObjectFromGUID("41f748")
+        
+        snapList = getObjectFromGUID("fc8314").getSnapPoints()
+        --getObjectFromGUID("5c23eb").takeObject({flip=true, position=snapList[100].position})
+
+        
+        getObjectFromGUID("5c23eb").takeObject(
+            {
+                position = getObjectFromGUID("fc8314").positionToWorld(snapList[37].position),
+                --flip = true DONT WORK
+            
+            }
+        )
     end
 end
